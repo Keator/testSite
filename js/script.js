67620133f5fbe58aba654f4b28f9adbe80d583e3
William Keator <31248990+Keator@users.noreply.github.com>
2020-07-01 13:28:32 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+  $('#slides').superslides({
+    animation: 'fade',
+    play: 8000,
+    pagination: false
+  });
+var typed = new Typed(".typed",
+{
+  strings: ["Web Programmer", "Database Developer", "Veteran"],
+  typeSpeed: 110,
+  loop: true,
+  startDelay: 1000,
+  showCursor: false
+});
+$('.owl-carousel').owlCarousel({
+      loop:true,
+      items:4,
+      responsive:{
+          0:{
+              items:1
+          },
+          480:{
+              items:2
+          },
+          768:{
+              items:3
+          },
+          938:{
+              items:4
+          }
+      }
+  });
+
+
+    var skillsTopOffset = $(".skillesSection").offset().top;
+    $(window).scroll(function() {
+
+  		if(window.pageYOffset > skillsTopOffset - $(window).height() + 250) {
+
+  			$('.chart').easyPieChart({
+  		        easing: 'easeInOut',
+  		        barColor: '#fff',
+  		        trackColor: false,
+  		        scaleColor: false,
+  		        lineWidth: 4,
+  		        size: 152,
+  		        onStep: function(from, to, percent) {
+  		        	$(this.el).find('.percent').text(Math.round(percent));
+  		        }
+  		    });
+
+
+  		}
+
+
+  	});
+
+
+});
